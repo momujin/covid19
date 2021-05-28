39f0e782c62caf9e29ce9ad667ff709795a3812d
momujin <juumonji@daily-tohoku.co.jp>
2021-05-28 18:47:47 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -22,26 +22,9 @@
       <whats-new2 class="mb-4" :items="eventItems" />
       <whats-new3 class="mb-4" :items="sportsItems"/>
       
-       <iframe src="https://e.infogram.com/c2903dda-3c88-45f5-802b-ee3c364ddd81?src=embed" title="ワクチン接種数" width="700"
-        height="2610" scrolling="no" frameborder="0" style="border:none;" allowfullscreen="allowfullscreen"></iframe>
-      <div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px;width: 640px">
-        <a href="https://infogram.com/c2903dda-3c88-45f5-802b-ee3c364ddd81"
-            style="color:#989898!important;text-decoration:none!important;" target="_blank">ワクチン接種数</a><br><a
-            href="https://infogram.com" style="color:#989898!important;text-decoration:none!important;" target="_blank"
-            rel="nofollow">Infogram</a>
-        </div>
-        
-        
-    <div class="col-md-6 col-12 infogram-embed" data-id="ba9944f3-2ecf-4f77-8f90-8203cd3663af" data-type="interactive"
-        data-title="ワクチン接種数（県別）"></div>
-    <script>!function (e, i, n, s) { var t = "InfogramEmbeds", d = e.getElementsByTagName("script")[0]; if (window[t] && window[t].initialized) window[t].process && window[t].process(); else if (!e.getElementById(n)) { var o = e.createElement("script"); o.async = 1, o.id = n, o.src = "https://e.infogram.com/js/dist/embed-loader-min.js", d.parentNode.insertBefore(o, d) } }(document, 0, "infogram-async");</script>
-    <div
-        style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px">
-        <a href="https://infogram.com/ba9944f3-2ecf-4f77-8f90-8203cd3663af"
-            style="color:#989898!important;text-decoration:none!important;" target="_blank">ワクチン接種数（県別）</a><br><a
-            href="https://infogram.com" style="color:#989898!important;text-decoration:none!important;" target="_blank"
-            rel="nofollow">Infogram</a>
-    </div>
+      <div class="DataCard col-md-6 col-12">
+          <iframe src="https://e.infogram.com/ba9944f3-2ecf-4f77-8f90-8203cd3663af?src=embed" title="ワクチン接種数（県別）" width="100%" height="550" scrolling="no" frameborder="0" style="border:none;" allowfullscreen="allowfullscreen"></iframe><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px;width: 640px"><a href="https://infogram.com/ba9944f3-2ecf-4f77-8f90-8203cd3663af" style="color:#989898!important;text-decoration:none!important;" target="_blank">ワクチン接種数（県別）</a><br><a href="https://infogram.com" style="color:#989898!important;text-decoration:none!important;" target="_blank" rel="nofollow">Infogram</a></div>
+      </div>
         
 
       <div class="WorryInfoBlock mb-4">
